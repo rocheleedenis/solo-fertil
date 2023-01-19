8665654259cb3ca30caf5a4f9e21fa2c9cce670b
Rochele Edenís <rochele.edenis@gmail.com>
2023-01-18 22:16:00 -0300

    refactor(NPKFertilizerInstallment): refactor the class

--- app/_src/dao/NPKFertilizerInstallment.php
+++ app/_src/dao/NPKFertilizerInstallment.php
@@ -1,22 +1,39 @@
 <?php
 
-	require_once '../../_connection/Connection.php';
+require_once '../../_connection/Connection.php';
+require_once '../lang/Translator.php';
 
 class NPKFertilizerInstallment
 {
-	public static function select($idCultura){
-		$database = Connection::connect();
+	/**
+	 * @param int $idCultura
+	 *
+	 * @return array
+	 */
+	public static function select($idCultura)
+	{
 		$query = "select nutriente, ciclo, porcentagem from parcelamentoAdubNPK where idCultura = :id";
-		try{
-			$pdo = $database->prepare($query);
+
+		try {
+			$pdo = self::prepareQuery($query);
+
 			$pdo->bindParam(":id", $idCultura, PDO::PARAM_INT);
-			if($pdo->execute()){
-				$oi = $pdo->fetchAll();
-			}
-			unset($database);
-			return $oi;
-		}catch(PDOException $e){
-			var_dump($e);
+
+			return $pdo->fetchAll();
+		} catch (PDOException $e) {
+			throw new \Exception(Translator::get('exceptions.query_execution.select'), 500);
 		}
+	}
+
+	/**
+	 * @param string $query
+	 *
+	 * @return PDOStatement
+	 */
+	private function prepareQuery($query)
+	{
+		$connection = Connection::connect();
+
+		return $connection->prepare($query);
 	}
 }
