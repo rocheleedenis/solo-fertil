384f1ee7000e136c8d21670e5c15f33227260b58
Rochele Edenís <rochele.edenis@gmail.com>
2023-01-19 21:14:47 -0300

    refactor(MineralFertilizationDao): refactor the class

--- app/_src/dao/MineralFertilizationDao.php
+++ app/_src/dao/MineralFertilizationDao.php
@@ -1,17 +1,38 @@
 <?php
 
-class MineralFertilizationDao {
-	public static function select($idCultura){
-		$database = Connection::connect();
+class MineralFertilizationDao
+{
+	/**
+	 * @param int $idCultura
+	 *
+	 * @return array|void
+	 */
+	public static function select($idCultura)
+	{
 		$query = "select disponibNutriente, p2o5soloArgiloso, p2o5soloMedio, p2o5soloArenoso, k2o, nitrogenio from adubacaoMineral where idCultura = :id";
+
 		try {
-			$pdo = $database->prepare($query);
+			$pdo = self::prepareQuery($query);
+
 			$pdo->bindParam(":id", $idCultura, PDO::PARAM_INT);
-			if ($pdo->execute()){
+
+			if ($pdo->execute()) {
 				return $pdo->fetchAll();
 			}
 		} catch (PDOException $e) {
-			var_dump($e->getMessage());
+			throw new \Exception(Translator::get('exceptions.query_execution.select'), 500);
 		}
+	}
+
+	/**
+	 * @param string $query
+	 *
+	 * @return PDOStatement
+	 */
+	private function prepareQuery($query)
+	{
+		$connection = Connection::connect();
+
+		return $connection->prepare($query);
 	}
 }
