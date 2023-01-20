2079d4d96b81634c70e098f41857dc4eadf5df48
Rochele Edenís <rochele.edenis@gmail.com>
2023-01-19 21:00:14 -0300

    refactor(CultivationDao): refactor the class

--- app/_src/dao/CultivationDao.php
+++ app/_src/dao/CultivationDao.php
@@ -1,79 +1,65 @@
 <?php
 
-    require_once '../../_connection/Connection.php';
-    require_once '../specials/SpecialCultura.php';
-    require_once '../models/ModelAdubacaoMineral.php';
-    require_once '../models/ModelParcelamentoAdubNPK.php';
+require_once '../../_connection/Connection.php';
+require_once '../specials/SpecialCultura.php';
+require_once '../models/ModelAdubacaoMineral.php';
+require_once '../models/ModelParcelamentoAdubNPK.php';
 
-class CultivationDao extends SpecialCultura{
+class CultivationDao extends SpecialCultura
+{
+	/**
+	 * @return array
+	 */
+	public static function selectAll()
+	{
+		$query = 'select id, nome, familia from cultura order by nome asc';
 
-	public static function selectAll(){
-		$database = Connection::connect();
-		$query = "select id, nome, familia from cultura order by nome asc";
-
-		try{
-			$pdo = $database->prepare($query);
-			$pdo->execute();
-			return $pdo->fetchAll();
-		}catch(PDOException $e){
-			var_dump($e->getMessage());
-		}
-	}
-
-	public static function selectSucessao($id){
-		$database = Connection::connect();
-		$query = "select cultura.id, nome, disponibNutriente, p2o5soloArgiloso, p2o5soloMedio, p2o5soloArenoso,
-			k2o, nitrogenio from cultura, adubacaoMineral where
-			cultura.id = adubacaoMineral.idCultura and familia <> (select familia from cultura where id = :id);";
 		try {
-			$pdo = $database->prepare($query);
-			$pdo->bindParam(":id", $id, PDO::PARAM_INT);
+			$pdo = self::prepareQuery($query);
 
 			$pdo->execute();
-			$oi = $pdo->fetchAll();
 
-			foreach ($oi as $key => $value) {
-				$a = $key;
-				if(((++$a)%4 == 1)){
-					$p=0;
-					$cult = $key;
-					$cult = (++$cult)/4;
-					$cult = (int)$cult;
-					$aux[$cult]['id']= $value['id'];
-					$aux[$cult]['nome']= $value['nome'];
-					$aux[$cult]['adubacao'][$p]['disponibNutriente'] = $value['disponibNutriente'];
-					$aux[$cult]['adubacao'][$p]['p2o5soloArgiloso'] = $value['p2o5soloArgiloso'];
-					$aux[$cult]['adubacao'][$p]['p2o5soloMedio'] = $value['p2o5soloMedio'];
-					$aux[$cult]['adubacao'][$p]['p2o5soloArenoso'] = $value['p2o5soloArenoso'];
-					$aux[$cult]['adubacao'][$p]['k2o'] = $value['k2o'];
-					$aux[$cult]['adubacao'][$p]['nitrogenio'] = $value['nitrogenio'];
-					unset($oi[$key]);
-					$p++;
-					$ei=0;
-				}else{
-					$aux[$cult]['adubacao'][$p]['disponibNutriente'] = $value['disponibNutriente'];
-					$aux[$cult]['adubacao'][$p]['p2o5soloArgiloso'] = $value['p2o5soloArgiloso'];
-					$aux[$cult]['adubacao'][$p]['p2o5soloMedio'] = $value['p2o5soloMedio'];
-					$aux[$cult]['adubacao'][$p]['p2o5soloArenoso'] = $value['p2o5soloArenoso'];
-					$aux[$cult]['adubacao'][$p]['k2o'] = $value['k2o'];
-					$aux[$cult]['adubacao'][$p]['nitrogenio'] = $value['nitrogenio'];
-					unset($oi[$key]);
-					$p++;
-				}
-			}
-			unset($oi);
-			return $aux;
+			return $pdo->fetchAll();
 		} catch (PDOException $e) {
-			var_dump($e->getMessage());
+			throw new \Exception(Translator::get('exceptions.query_execution.select'), 500);
 		}
 	}
 
-	public function selectOne($id){
-		$database = Connection::connect();
-		$query = "select * from cultura where id = :id";
+	/**
+	 * @param int $id
+	 *
+	 * @return array
+	 */
+	public static function selectSucessao($id)
+	{
+		$query = 'select cultura.id, nome, disponibNutriente, p2o5soloArgiloso, p2o5soloMedio, p2o5soloArenoso,
+			k2o, nitrogenio from cultura, adubacaoMineral where
+			cultura.id = adubacaoMineral.idCultura and familia <> (select familia from cultura where id = :id);';
+
+		try {
+			$pdo = self::prepareQuery($query);
+
+			$pdo->bindParam(':id', $id, PDO::PARAM_INT);
+
+			$pdo->execute();
+
+			$data = $pdo->fetchAll();
+
+			return self::formatData($data);
+		} catch (PDOException $e) {
+			throw new \Exception(Translator::get('exceptions.query_execution.select'), 500);
+		}
+	}
+
+	public function selectOne($id)
+	{
+		$query = 'select * from cultura where id = :id';
+
 		try{
-			$pdo = $database->prepare($query);
-			$pdo->bindParam(":id", $id, PDO::PARAM_INT);
+			$pdo = self::prepareQuery($query);
+
+			$pdo->bindParam(':id', $id, PDO::PARAM_INT);
+
 			if ($pdo->execute()){
 				$oi = $pdo->fetch();
 
@@ -95,10 +81,59 @@ class CultivationDao extends SpecialCultura{
 
 				$this->setParcNPKtable(ParcelamentoAdubNPK::select($this->getId()));
 			}else{
-				echo "Não foi possivel recuperar dados. Verificar erro.";
+				echo 'Não foi possivel recuperar dados. Verificar erro.';
 			}
 		}catch(PDOException $e){
-			var_dump($e->getMessage());
+			throw new \Exception(Translator::get('exceptions.query_execution.select'), 500);
 		}
+	}
+
+	/**
+	 * @param string $query
+	 *
+	 * @return PDOStatement
+	 */
+	private function prepareQuery($query)
+	{
+		$connection = Connection::connect();
+
+		return $connection->prepare($query);
+	}
+
+	/**
+	 * @param array $data
+	 *
+	 * @return array
+	 */
+	private function formatData($data)
+	{
+		$aux = [];
+		$a = null;
+		$position = 0;
+		$cult = 0;
+
+		foreach ($data as $index => $cultivation) {
+			$a = $index;
+
+			if ((++$a) % 4 == 1) {
+				$position = 0;
+				$cult = $index;
+				$cult = (++$cult) / 4;
+				$cult = (int) $cult;
+				$aux[$cult]['id'] = $cultivation['id'];
+				$aux[$cult]['nome'] = $cultivation['nome'];
+			}
+
+			$aux[$cult]['adubacao'][$position]['disponibNutriente'] = $cultivation['disponibNutriente'];
+			$aux[$cult]['adubacao'][$position]['p2o5soloArgiloso'] = $cultivation['p2o5soloArgiloso'];
+			$aux[$cult]['adubacao'][$position]['p2o5soloMedio'] = $cultivation['p2o5soloMedio'];
+			$aux[$cult]['adubacao'][$position]['p2o5soloArenoso'] = $cultivation['p2o5soloArenoso'];
+			$aux[$cult]['adubacao'][$position]['k2o'] = $cultivation['k2o'];
+			$aux[$cult]['adubacao'][$position]['nitrogenio'] = $cultivation['nitrogenio'];
+
+			$position++;
+		}
+
+		return $aux;
 	}
 }
